cc42f36fba563f55e467cfbdb200e1ace0683b2e
DiamonickKCS <46506748+diamonick@users.noreply.github.com>
2024-08-17 12:09:08 -0500

    Implement Inter font family and define screens, width, border radius, and drop shadow CSS variables in config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,72 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend:
+    {
+      screens:
+      {
+        'mobile-sm': '320px',
+        // => @media (min-width: 320px) { ... }
+        
+        'mobile-md': '375px',
+        // => @media (min-width: 375px) { ... }
+
+        'mobile-lg': '425px',
+        // => @media (min-width: 425px) { ... }
+
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+        
+        'md-lg': '896px',
+        // => @media (min-width: 896px) { ... }
+  
+        'lg': '1080px',
+        // => @media (min-width: 1080px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
+      fontFamily:
+      {
+        'Inter': ['Inter Regular'],
+        'Inter-L': ['Inter Light'],
+        'Inter-M': ['Inter Medium'],
+        'Inter-SB': ['Inter SemiBold'],
+        'Inter-B': ['Inter Bold'],
+        'Inter-EXB': ['Inter ExtraBold'],
+        'Inter-BLK': ['Inter Black']
+      },
+      width:
+      {
+        'col-1': '8.33%',
+        'col-2': '16.66%',
+        'col-3': '25%',
+        'col-4': '33.33%',
+        'col-5': '41.66%',
+        'col-6': '50%',
+        'col-7': '58.33%',
+        'col-8': '66.66%',
+        'col-9': '75%',
+        'col-10': '83.33%',
+        'col-11': '91.66%',
+        'col-12': '100%'
+      },
+      borderRadius:
+      {
+        'circular': '50%'
+      },
+      dropShadow:
+      {
+        'default': '0 16px 32px rgba(0, 0, 0, 0.25)'
+      },
+    },
   },
   plugins: [],
 }
