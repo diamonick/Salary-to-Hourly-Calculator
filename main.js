e79b6a7ab0d25791523c8504bd12e49123d2ff8d
DiamonickKCS <46506748+diamonick@users.noreply.github.com>
2024-08-19 20:33:05 -0500

    Implement calculating hourly pay, monthly pay, weekly pay, and % of US median salary

--- main.js
+++ main.js
@@ -158,6 +158,12 @@ class InputField
         this.element.addEventListener(event, functionCall);
     }
 
+    // Get value displayed in input field.
+    getValue()
+    {
+        return this.element.value;
+    }
+
     // Enable input field.
     enableInputField()
     {
@@ -208,10 +214,17 @@ class InputField
 }
 
 //#region Variables
-const calculateButton = new Button(".Calculate-Button", "#0071e3", "#00A3FF", "#004fbd");
 const salaryField = new InputField(".Salary-Field");
 const hoursPerWeekField = new InputField(".Hours-Per-Week-Field");
 const weeksPerYearField = new InputField(".Weeks-Per-Year-Field");
+const calculateButton = new Button(".Calculate-Button", "#0071e3", "#00A3FF", "#004fbd");
+
+const hourlyPayAmount = getElement(".Hourly-Pay .Amount");
+const monthlyPayAmount = getElement(".Monthly-Pay .Amount");
+const weeklyPayAmount = getElement(".Weekly-Pay .Amount");
+const percentUSMedianSalaryAmount = getElement(".Percent-US-Median-Salary .Amount");
+
+const USMedianSalary = 59228;
 //#endregion
 
 function Main()
@@ -231,7 +244,85 @@ function onHoverInputField(inputField)
 
 function calculateResults()
 {
+    let hourlyPayAmountText, monthlyPayAmountText, weeklyPayAmountText, percentUSMedianSalaryAmountText;
 
+    let hourlyPay = calculateHourlyPay();
+    let monthlyPay = calculateMonthlyPay();
+    let weeklyPay = calculateWeeklyPay();
+    let percentOfUSMedianSalary = calculatePercentOfUSMedianSalary();
+
+    hourlyPayAmount.innerHTML = `$${hourlyPay}`;
+    monthlyPayAmount.innerHTML = `$${monthlyPay}`;
+    weeklyPayAmount.innerHTML = `$${weeklyPay}`;
+    percentUSMedianSalaryAmount.innerHTML = `${percentOfUSMedianSalary}%`;
+}
+
+function calculateHourlyPay()
+{
+    let salary = salaryField.getValue();                        // Salary per year
+    let hoursPerWeek = hoursPerWeekField.getValue();            // Hours worked per week
+    let weeksPerYear = weeksPerYearField.getValue();            // Weeks worked per year
+
+    const rawResult = salary / (hoursPerWeek * weeksPerYear);
+
+    // Round up/down hourly pay by 2 decimal places.
+    const result = Math.round(rawResult * 100) / 100;
+    console.log(`Hourly Pay: $${result}`);
+    return result;
+}
+
+function calculateMonthlyPay()
+{
+    let salary = salaryField.getValue();                        // Salary per year
+    const numOfMonths = 12;                                     // Total number of months in a year.
+
+    const rawResult = salary / numOfMonths;
+
+    // Round up/down monthly pay by 2 decimal places.
+    const result = Math.round(rawResult * 100) / 100;
+    console.log(`Monthly Pay: $${result}`);
+    return result;
+}
+
+function calculateWeeklyPay()
+{
+    let salary = salaryField.getValue();                        // Salary per year
+    let weeksPerYear = weeksPerYearField.getValue();            // Weeks worked per year
+
+    const rawResult = salary / weeksPerYear;
+
+    // Round up/down weekly pay by 2 decimal places.
+    const result = Math.round(rawResult * 100) / 100;
+    console.log(`Weekly Pay: $${result}`);
+    return result;
+}
+
+function calculatePercentOfUSMedianSalary()
+{
+    let salary = salaryField.getValue();                        // Salary per year
+
+    const rawResult = (salary / USMedianSalary) * 100;
+
+    // Round up/down % of US median salary by 1 decimal place.
+    const result = Math.round(rawResult * 10) / 10;
+    console.log(`% of US Median Salary: ${result}%`);
+    return result;
+}
+
+function shuffleText(text, prefix, suffix)
+{
+    var a = text.split(""),
+        n = a.length;
+
+    for (var i = n - 1; i > 0; i--)
+    {
+        var j = Math.floor(Math.random() * (i + 1));
+        var tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+    return a.join("");
 }
 
 Main();
